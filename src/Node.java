62b97e4544eabf54864e7a9085e5fa98577c0705
lyes <lyes.makhloufi@outlook.fr>
2020-07-01 09:43:53 +0200

    first commit, class node and binary tree

---
+++ src/Node.java
@@ -0,0 +1,14 @@
+
+public class Node {
+	
+	private int value;
+	private Node left;
+	private Node right;
+	
+	public Node(int value) {
+		this.value = value;
+		right = null;
+		left = null;
+	}
+
+}
